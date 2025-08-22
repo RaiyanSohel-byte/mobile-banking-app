463f6f28616c2c845bdea36b5ec8a440ccc778bb
RaiyanSohel-byte <afnan19262@gmail.com>
2025-08-22 19:48:25 +0600

    login page design added

---
+++ index.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById("login-btn");
+// login button functionality
+loginButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  //   console.log("click");
+});
