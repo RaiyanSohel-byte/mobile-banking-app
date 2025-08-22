a8b5c9cd2f0e6cb1f632675bca1536005c72bd60
RaiyanSohel-byte <afnan19262@gmail.com>
2025-08-22 23:51:52 +0600

    add money popdown added

--- home.js
+++ home.js
@@ -5,5 +5,15 @@ addMoneyButton.addEventListener("click", (event) => {
   event.preventDefault();
   const amountInput = parseInt(document.getElementById("amount-input").value);
   document.getElementById("balance").innerText = `$${amountInput + balance}`;
-  alert(`$${amountInput} Added!!`);
+  document.getElementById(
+    "popup"
+  ).innerHTML = `<div role="alert" class="alert alert-success">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>$${amountInput} Added to your account!</span>
+</div>`;
+  setTimeout(() => {
+    document.getElementById("popup").innerHTML = ``;
+  }, 2000);
 });
