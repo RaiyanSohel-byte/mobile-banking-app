f8c48f73510ea957bad514b70b1f46d2e8573a90
RaiyanSohel-byte <afnan19262@gmail.com>
2025-08-23 13:20:07 +0600

    dollar sign changed to taka

--- home.js
+++ home.js
@@ -75,7 +75,7 @@ addMoneyButton.addEventListener("click", (event) => {
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
-  <span>$${amountInput} Added to your account!</span>
+  <span>৳${amountInput} Added to your account!</span>
 </div>`;
     setTimeout(() => {
       document.getElementById("popup").innerHTML = ``;
@@ -124,7 +124,19 @@ withdrawMoneyButton.addEventListener("click", (event) => {
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
-  <span>$${amntInput} Withdrawn from your account!</span>
+  <span>৳${amntInput} Withdrawn from your account!</span>
+</div>`;
+    setTimeout(() => {
+      document.getElementById("popup").innerHTML = ``;
+    }, 2000);
+  } else if (amntInput > balance) {
+    document.getElementById(
+      "popup"
+    ).innerHTML = `<div role="alert" class="alert alert-error">
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+  <span>Insufficient Balance!!</span>
 </div>`;
     setTimeout(() => {
       document.getElementById("popup").innerHTML = ``;
